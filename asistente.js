c7c5cb4704e02e4308da122e119dd92572295f73
Rev3rsal <112159883+Rev3rsal@users.noreply.github.com>
2023-03-25 14:37:51 -0300

    Actualizacion

---
+++ asistente.js
@@ -0,0 +1,26 @@
+let sound = new Audio("megaman.mp3")
+sound.volume= 0.5
+
+let megaman = document.getElementById("potato4");
+
+/*if (window.matchMedia("(min-width: 800px)").matches){
+efecto.addEventListener("mouseover", () => {
+    afectado.style.display="none"
+    afectado2.style.display="block"
+    afectado2.style.opacity=1
+    afectado3.style.opacity=1
+    sound.play()
+  }, false)
+  efecto.addEventListener("mouseout", () => {
+    afectado.style.display="block"
+    afectado2.style.display="none"
+    afectado2.style.opacity=0
+    afectado3.style.opacity=0
+  }, false)
+}
+megaman.onclick = teletransporte;
+
+function teletransporte() {
+    sound;
+}
+*/
